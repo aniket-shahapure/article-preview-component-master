5cf63cb1a7d8e76b1aa987c90d2d850bf6207456
aniket-shahapure <aniketshahapure17@gmail.com>
2026-03-03 01:51:09 +0530

    using HTML-CSS-JS

---
+++ box.js
@@ -0,0 +1,11 @@
+console.log("hello");
+
+function share(){
+    document.getElementById("share-1").style.display="none";
+    document.getElementById("share-2").style.display="flex";
+
+}
+function  subShare(){
+    document.getElementById('share-1').style.display="flex";
+    document.getElementById('share-2').style.display="none";
+}
